4be1297402086d91a0dee4c96a0693e2888cb54c
ginoLanetti <Alfonsichqu@gmail.com>
2019-05-17 16:20:57 +0200

    first of final project

---
+++ finalProject/index.js
@@ -0,0 +1,9 @@
+const links = [{
+    title: 'Facebook',
+    url: 'http://www.facebook.com',
+    author: 'Me'
+}, {
+    title: 'Instagram',
+    url: 'http://www.instagram.com',
+    author: 'Also Me'
+}];
